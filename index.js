b155f42e92e4c00612a239690dd34fb526f2dccf
begonia950807-oss <begonia950807@gmail.com>
2026-03-12 20:21:25 +0800

    Update text content and formatting in index.js

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text , View }from "react-native";
 
 export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+    return (
+        <View style={styles.container}>
+        <View style={styles.main}>
+        <Text style={styles.title}>腳肘其實就是膝蓋</Text>
+        <Text style={styles.subtitle}>姓名:林珏佑  學號:111307030</Text>
+        </View>
       </View>
-    </View>
-  );
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+ const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        padding: 24,
+    },
+    main: {
+        flex: 1,
+        justifyContent: "center",
+        maxWidth: 960,
+        marginHorizontal: "auto",
+    },
+    title: {
+        fontSize: 64,
+        fontWeight: "bold",
+    },
+    subtitle: {
+        fontSize: 36,
+        color:" #D6E1D7 ",
+    },
+ });
