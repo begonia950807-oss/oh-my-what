353cd86b001f361b9ccf550646ec957b8ae94f80
begonia950807-oss <begonia950807@gmail.com>
2026-03-12 22:00:35 +0800

    Update subtitle text with student information

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ export default function Page() {
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text style={styles.subtitle}>姓名：林珏佑 學號：111307030 </Text>
       </View>
     </View>
   );
